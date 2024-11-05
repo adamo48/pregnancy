a5fbb985e7d6563d0011394bc7cdd19711f095b5
Rufna <149588517+Rufna@users.noreply.github.com>
2024-11-05 13:20:40 +0100

    dfghjk

--- pregnancy.py
+++ pregnancy.py
@@ -1,4 +1,10 @@
-pass
-
-
-
+def cost_ginecologist():
+    #how many visits* medium cost of private visit in the city
+def cost_prenatal_tests():
+    ...
+def cost_supplements():
+    ...
+def cost_labour():
+    # cost of different painreliefs
+    #cost of private labour
+    #cost of nfz labour
