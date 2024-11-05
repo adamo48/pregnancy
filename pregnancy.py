de9a5442fb2da88732820a3fb1c391c203ea90bb
Rufna <149588517+Rufna@users.noreply.github.com>
2024-11-05 13:53:09 +0100

    link

--- pregnancy.py
+++ pregnancy.py
@@ -1,5 +1,12 @@
 def cost_ginecologist():
     #pakiet medyczny średni koszt
+    luxmed=https://www.luxmed.pl/dla-pacjenta/abonamenty/abonamenty-dla-doroslych-i-dzieci/pakiet-kompleksowy-zloty
+#ciąża na luxmed, mniejszy pakiet
+    medicover = https://www.medistore.com.pl/p/pakiet-szpitalny-prowadzenia-ciazy-3-trymestry?utm_source=porody&utm_medium=page&utm_campaign=sale&utm_content=buy%20now
+#ciąża na medicover, więcej pieniędzy, ale bardziej rozbudowany
+#na NFZ za darmo przy prawidłowym przebiegu
+NIPT = https://diag.pl/sklep/pakiety/sanco-test-prenatalny/
+#dodatkowy NIPT bo nikt tego nie refunduje
 def cost_prenatal_tests():
     #badania nie w pakiecie, płatne 
 def cost_supplements():
