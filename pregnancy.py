75ab925efd5f92180c99d942655413d881ee5f66
adamo48 <adamo48@o2.pl>
2024-11-05 13:56:15 +0100

    adasko

--- pregnancy.py
+++ pregnancy.py
@@ -5,9 +5,9 @@ def cost_ginecologist():
     medicover = https://www.medistore.com.pl/p/pakiet-szpitalny-prowadzenia-ciazy-3-trymestry?utm_source=porody&utm_medium=page&utm_campaign=sale&utm_content=buy%20now
 #ciąża na medicover, więcej pieniędzy, ale bardziej rozbudowany
 #na NFZ za darmo przy prawidłowym przebiegu
-NIPT = https://diag.pl/sklep/pakiety/sanco-test-prenatalny/
-#dodatkowy NIPT bo nikt tego nie refunduje
 def cost_prenatal_tests():
+    NIPT = https://diag.pl/sklep/pakiety/sanco-test-prenatalny/
+    #dodatkowy NIPT bo nikt tego nie refunduje
     #badania nie w pakiecie, płatne 
 def cost_supplements():
     #zalecane suplementy
