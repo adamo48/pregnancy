33682fb8c660b4c2a79a4edc4efaf5d6dd01baea
Rufna <149588517+Rufna@users.noreply.github.com>
2024-11-08 15:37:44 +0100

    FixLuxmed

--- pregnancy.py
+++ pregnancy.py
@@ -3,10 +3,23 @@ from bs4 import BeautifulSoup
 
 def main():
     print(cost_ginecologist())
-
+...
 def cost_ginecologist():
     # pakiet medyczny sredni koszt
-    luxmed = "https://www.luxmed.pl/dla-pacjenta/abonamenty/abonamenty-dla-doroslych-i-dzieci/pakiet-kompleksowy-zloty"
+    from string import ascii_letters
+    luxmed="https://www.luxmed.pl/dla-pacjenta/abonamenty/abonamenty-dla-doroslych-i-dzieci/pakiet-kompleksowy-zloty"
+    headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko)' 'Chrome/41.0.2227.1 Safari/537.36'}
+    responsee = requests.get(luxmed, headers=headers)
+    soupo = BeautifulSoup(responsee.text, "html.parser")
+    cena = soupo.find("div", class_="details__price")
+    cena = cena.text
+    cena = cena.replace(" ", "")
+    cena = cena.strip(ascii_letters)
+    cena = cena.replace("zÅ", "")
+   # if cena:
+    print("Cena pakietu luxmed:", cena)
+    #else:
+            #print("Nie znaleziono ceny.")
     # ciaza na luxmed, mniejszy pakiet
     medicover = "https://www.medistore.com.pl/p/pakiet-szpitalny-prowadzenia-ciazy-3-trymestry?utm_source=porody&utm_medium=page&utm_campaign=sale&utm_content=buy%20now"
     response = requests.get(medicover)
