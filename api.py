4a1daf46572f1f247bf9ed162e63fb08fda756a6
rufna <rufna@interia.pl>
2024-11-20 17:49:43 +0100

    api

---
+++ api.py
@@ -0,0 +1,33 @@
+import requests
+import json
+import datetime
+
+# URL do pobrania danych
+year = datetime.datetime.now()
+year=year.year
+x=0
+for x in range(11):
+      year = year-1
+      x+1
+      url = "https://api-sdp.stat.gov.pl/api/1.0.0/variable/variable-data-section?id-zmienna=305&id-przekroj=739&id-rok=:rok:&id-okres=282&page-size=5000&page=0&lang=pl"
+      url = url.replace(":rok:", str(year))
+      print(url)
+headers = {
+    'X-ClientId': 'qSWq2WGJXOVsxXFcn8IJ8DYobLAbEMC73IBynhwf3RM='  # Twój klucz API
+}
+
+
+params = {
+    'format': 'json',  # Format odpowiedzi
+}
+
+response = requests.get(url, headers=headers)
+
+if response.status_code == 200:
+    data = response.json()
+
+target_row = next((row for row in data["data"] if row["rownumber"] == 1), None)
+if target_row:
+        print("Inflacja: ", float(target_row["wartosc"])-100)
+else:
+        print("Nie znaleziono wskaźnika.")    
