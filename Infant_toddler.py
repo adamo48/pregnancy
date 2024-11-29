fb9bd8f0276e3b8b2c6eb41856305458ee01d3da
rufna <rufna@interia.pl>
2024-11-29 16:43:11 +0100

    move api to Infant_toddler file

--- Infant_toddler.py
+++ Infant_toddler.py
@@ -1,4 +1,35 @@
-from api import get_inflation, get_url
+import requests
+import json
+import datetime
+
+def get_url():
+    year = datetime.datetime.now()
+    year=year.year
+    list_years_url=[]
+
+    for x in range(10):
+        year -=1
+        url = "https://api-sdp.stat.gov.pl/api/1.0.0/variable/variable-data-section?id-zmienna=305&id-przekroj=739&id-rok=:rok:&id-okres=282&page-size=5000&page=0&lang=pl"
+        url = url.replace(":rok:", str(year))
+        list_years_url.append(url)
+        
+    return list_years_url
+    
+def get_inflation(list_years_url):
+    list_url = []
+    for url in list_years_url:
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            if target_row:= next((row for row in data["data"] if row["rownumber"] == 1), None):
+                    t = float(target_row["wartosc"])-100
+                    list_url.append(t)
+            else:
+                 return("Error")        
+        else:
+            return("Couldn't find the indicator. Try again later.")    
+    return(sum(list_url)/10)
+
 inflation = get_inflation(get_url())
 inflation = float(inflation)/100
 print(inflation)
