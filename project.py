bdf930f555b1d27cd68282cf367397d35e7b6a0e
rufna <rufna@interia.pl>
2024-12-03 15:19:14 +0100

    polishd project

---
+++ project.py
@@ -0,0 +1,285 @@
+import requests
+from bs4 import BeautifulSoup
+from tools import *
+import datetime
+
+def main():
+    print(f"Cost of ginecologist: {cost_ginecologist()} PLN")
+    print(f"Cost of recommended supplements: {cost_supplements()} PLN")
+    print(f"Cost of prenatal test that aren't covered: {cost_prenatal_tests()} PLN")
+    print(f"Cost of labour: {cost_labour()} PLN")
+    print(f"Cost of raising a child: {cost_child_stages()} PLN")
+def cost_ginecologist():
+    # pakiet medyczny sredni koszt
+    luxmed="https://www.luxmed.pl/dla-pacjenta/abonamenty/abonamenty-dla-doroslych-i-dzieci/pakiet-kompleksowy-zloty"
+    headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko)' 'Chrome/41.0.2227.1 Safari/537.36'}
+    responsee = requests.get(luxmed, headers=headers)
+    soupo = BeautifulSoup(responsee.text, "html.parser")
+    cena = soupo.find("div", class_="details__price")
+    cena = cena.text
+    cena = cena.replace("zÅ\x82", "").replace("Cena od ", "")
+    cena = float(cena)*9
+    # ciaza na luxmed, mniejszy pakiet
+    medicover = "https://www.medistore.com.pl/p/pakiet-szpitalny-prowadzenia-ciazy-3-trymestry?utm_source=porody&utm_medium=page&utm_campaign=sale&utm_content=buy%20now"
+    response = requests.get(medicover)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    price_medicover = soup.find("span", class_="price-field__value")
+    price_medicover = price_medicover.text
+    price_medicover = price_medicover.replace('\n',''). replace(' ','').replace('od','').replace('zł','').replace(',','.')
+    price_medicover = float(price_medicover)
+    while True:
+        what_kind_prenatal_care = input("Do you want to calculate cost of private medical care? yes|no").lower()
+        if what_kind_prenatal_care == "yes":
+            print("What kind of package do you choose? \nLux-medium size, you might have to pay a little extra for additional tests/exams. \nMed-should cover almost all your needs, which can be helpful with difficult pregnancy.")
+            while True:
+                package = input("Choose lux|med: ").lower()
+                if package in {'lux','med'}:
+                    if package == 'lux':
+                        return cena
+                    elif package == 'med':
+                        return price_medicover    
+                print("Incorrect. Answear lux or med.")
+        elif what_kind_prenatal_care == "no":
+            cost_nfz = 0
+            return cost_nfz
+        print("Incorrect. Try again.")
+     
+    #ciaza na medicover, wiecej pieniedzy, ale bardziej rozbudowany
+    #na NFZ za darmo przy prawidlowym przebiegu
+def cost_prenatal_tests():
+    nipt = "https://diag.pl/sklep/pakiety/sanco-test-prenatalny/"
+    response = requests.get(nipt)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    price = soup.find("div", class_="MuiTypography-root MuiTypography-h3 css-53tl63")
+    # dodatkowy NIPT bo nikt tego nie refunduje
+    # badania nie w pakiecie, platne 
+    price = price.string
+    price = price.replace(',','.').replace(' ','').replace('PLN','')
+    price = float(price)
+    return price
+
+def cost_supplements():
+    iron_ = Supplements(iron)
+    iron_.fetch_price()
+    iron_.price = iron_.price*3
+    folic = Supplements(folic_acid)
+    folic.fetch_price()
+    folic.price = folic.price*3
+    wit = Supplements(witD)
+    wit.fetch_price()
+    wit.price = wit.price*6
+    dHa = Supplements(dha)
+    dHa.fetch_price()
+    dHa.price = dHa.price*6
+    jod_1 = Supplements(jod1)
+    jod_1.fetch_price()
+    jod_1.price = jod_1.price*3
+    jod_2 = Supplements(jod2)
+    jod_2.fetch_price()
+    jod_2.price = jod_2.price*6
+    return iron_.price + folic.price + wit.price + dHa.price + jod_1.price + jod_2.price
+    # zalecane suplementy
+def cost_labour():
+    private_natural_labour_cost = 9015
+    private_emperors_cut_cost = 6245
+    nfz_cost = 0
+    labour_financing_type = get_labour_financing_type()
+    if labour_financing_type == 'nfz':
+
+        labour_type = get_labour_type()
+        nfz_options = {
+        1: {'description': 'Additional midwife care', 'price': 1500},
+        2: {'description': 'Cost of private room/family room', 'price': 500},
+        3: {'description': 'Presence of family member during cesarean section', 'price': 200},
+        4: {'description': 'Additional doctors care during birth', 'price': 2000}
+    }
+        display_nfz_options(nfz_options)
+        selected_nfz_options = get_user_selection(nfz_options)
+
+        nfz_addons_cost = calculate_nfz_addons_cost(selected_nfz_options, nfz_options)
+        nfz_cost += nfz_addons_cost
+        return nfz_cost
+    
+    labour_type = get_labour_type()
+    if labour_type == 'natural':
+        return private_natural_labour_cost
+    elif labour_type == 'cc':
+        return private_emperors_cut_cost
+    
+    # cost of different painreliefs
+    # cost of private labour
+    
+    # cost of nfz labour
+    
+def get_url():
+    year = datetime.datetime.now()
+    year=year.year
+    list_years_url=[]
+
+    for x in range(10):
+        year -=1
+        url = "https://api-sdp.stat.gov.pl/api/1.0.0/variable/variable-data-section?id-zmienna=305&id-przekroj=739&id-rok=:rok:&id-okres=282&page-size=5000&page=0&lang=pl"
+        url = url.replace(":rok:", str(year))
+        list_years_url.append(url)
+        
+    return list_years_url
+    
+def get_inflation(list_years_url):
+    list_url = []
+    for url in list_years_url:
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            if target_row:= next((row for row in data["data"] if row["rownumber"] == 1), None):
+                    t = float(target_row["wartosc"])-100
+                    list_url.append(t)
+            else:
+                 return("Error")        
+        else:
+            raise ValueError("Couldn't find the indicator. Try again later.")    
+    inflation = (sum(list_url)/10)
+    inflation = float(inflation/100)
+    return inflation
+
+inflation = get_inflation(get_url())
+
+def calc_newborn():
+
+    infant = Child_budget("Newborn")  
+    for keys, values in newborn.items():      
+        infant.add_expenses({keys},{values})
+    for key, value in newborn.items():  
+        print(f"{key}: {value}")
+    total_newborn = sum(newborn.values())
+    years = 1
+    total_newborn*=(1+inflation)**years
+    return total_newborn
+
+def calc_toddler():
+    small_child = Child_budget("Toddler")  
+    for keys, values in toddler.items():      
+        small_child.add_expenses({keys},{values})
+    for key, value in toddler.items():  
+        print(f"{key}: {value}")
+    total_toddler = sum(toddler.values())
+    years = 1
+    total_toddler*=(1+inflation)**years
+    return total_toddler
+
+def calc_older_toddler():
+    small_child_2 = Child_budget("Toddler 4-6")  
+    for keys, values in older_toddler.items():      
+        small_child_2.add_expenses({keys},{values})
+    for key, value in older_toddler.items():  
+        print(f"{key}: {value}")
+    total_older_toddler = sum(older_toddler.values())
+    years = 2
+    total_older_toddler*=(1+inflation)**years
+    return total_older_toddler 
+
+def calc_preschool():
+    preschooler = Child_budget("Preschooler")  
+    for keys, values in preschool.items():      
+        preschooler.add_expenses({keys},{values})
+    for key, value in preschool.items():  
+        print(f"{key}: {value}")
+    total_preschool = sum(preschool.values())
+    years = 3
+    total_preschool*=(1+inflation)**years
+    return total_preschool 
+
+def calc_child():
+    a_child = Child_budget("Child")  
+    for keys, values in child.items():      
+        a_child.add_expenses({keys},{values})
+    for key, value in child.items():  
+        print(f"{key}: {value}")
+    total_child = sum(child.values())
+    years = 6
+    total_child*=(1+inflation)**years
+    return total_child
+
+def calc_teen():
+    teenager = Child_budget("Teenager")  
+    for keys, values in teen.items():      
+        teenager.add_expenses({keys},{values})
+    for key, value in teen.items():  
+        print(f"{key}: {value}")
+    total_teen = sum(teen.values())
+    years = 5
+    total_teen*=(1+inflation)**years
+    return total_teen
+
+def cost_child_stages():    
+    stage_of_child = input("Do you currently have a child? Type yes|no ").lower()
+    private=input("Do you want to calculate cost of private schools? Type yes|no ").lower()
+    if stage_of_child=="yes":
+        what_stage=int(input("Input age of a child in years: "))
+        if what_stage<1:
+            if private =="yes":
+                del preschool["public_preschool"]
+                del child["public_school"]
+                del teen["public_school"]
+                return(f"Total cost with inflation throught years until 18: {calc_newborn()+calc_toddler()+calc_older_toddler()+calc_preschool()+calc_child()+calc_teen():.2f} PLN")
+
+            else:
+                del preschool["private_preschool"]
+                del child["private_school"]
+                del teen["private_school"]
+                return(f"Total cost with inflation throught years until 18: {calc_newborn()+calc_toddler()+calc_older_toddler()+calc_preschool()+calc_child()+calc_teen():.2f} PLN")
+        elif 0<what_stage<4:
+            if private =="yes":
+                del preschool["public_preschool"]
+                del child["public_school"]
+                del teen["public_school"]
+                return(f"Total cost with inflation throught years until 18: {calc_older_toddler()+calc_preschool()+calc_child()+calc_teen():.2f} PLN")
+            else:
+                del preschool["private_preschool"]
+                del child["private_school"]
+                del teen["private_school"]
+                return(f"Total cost with inflation throught years until 18: {calc_older_toddler()+calc_preschool()+calc_child()+calc_teen():.2f} PLN")
+        elif 3<what_stage<7:
+            if private =="yes":
+                del preschool["public_preschool"]
+                del child["public_school"]
+                del teen["public_school"]
+                return(f"Total cost with inflation throught years until 18: {calc_preschool()+calc_child()+calc_teen():.2f} PLN")
+            else:
+                del preschool["private_preschool"]
+                del child["private_school"]
+                del teen["private_school"]
+                return(f"Total cost with inflation throught years until 18: {calc_preschool()+calc_child()+calc_teen():.2f} PLN")
+        elif 6<what_stage<13:
+            if private =="yes":
+                del child["public_school"]
+                del teen["public_school"]
+                return(f"Total cost with inflation throught years until 18: {calc_child()+calc_teen():.2f} PLN")
+            else:
+                del child["private_school"]
+                del teen["private_school"]
+                return(f"Total cost with inflation throught years until 18: {calc_child()+calc_teen():.2f} PLN")
+        elif 12<what_stage<19:
+            if private =="yes":
+                del teen["public_school"]
+                return(f"Total cost with inflation throught years until 18: {calc_teen():.2f} PLN")
+            else:
+                del teen["private_school"]
+                return(f"Total cost with inflation throught years until 18: {calc_teen():.2f} PLN")              
+    elif stage_of_child == "no":
+        if private =="yes":
+                del preschool["public_preschool"]
+                del child["public_school"]
+                del teen["public_school"]
+                return(f"Total cost with inflation throught years until 18: {calc_newborn()+calc_toddler()+calc_older_toddler()+calc_preschool()+calc_child()+calc_teen():.2f} PLN")
+
+        else:
+                del preschool["private_preschool"]
+                del child["private_school"]
+                del teen["private_school"]
+                return(f"Total cost with inflation throught years until 18: {calc_newborn()+calc_toddler()+calc_older_toddler()+calc_preschool()+calc_child()+calc_teen():.2f} PLN")
+
+           
+
+
+if __name__ == "__main__":
+    main()
--- tools.py
+++ tools.py
@@ -3,20 +3,20 @@ from bs4 import BeautifulSoup
 import re
 def get_labour_financing_type():
     while True:
-        labour_financing_type = input('Decydujesz się na poród prywatny czy NFZ? (priv/nfz): ').lower()
+        labour_financing_type = input('Birth on nfz or private? (priv/nfz): ').lower()
         if labour_financing_type in {'priv', 'nfz'}:
             return labour_financing_type
-        print('Wprowadzono niepoprawne dane, spróbuj ponownie')
+        print('Incorrect. Try again.')
 
 def get_labour_type():
     while True:
-        labour_type = input('Poród naturalny czy cesarskie cięcie? (natura/cc)').lower()
-        if labour_type in {'natura','cc'}:
+        labour_type = input('Natural birth or cc? (natura/cc)').lower()
+        if labour_type in {'natural','cc'}:
             return labour_type
-        print('Wprowadzono niepoprawne dane, spróbuj ponownie')
+        print('Incorrect. Try again.')
 
 def display_nfz_options(options):
-    print("Dostępne opcje dodatkowe:")
+    print("Availiable additional options:")
     for key, value in options.items():
         print(f"{key}. {value['description']} - {value['price']} zł")
     print()
@@ -25,16 +25,16 @@ def get_user_selection(options):
     selected_options = []
     while True:
         try:
-            choice = int(input("Wybierz numer opcji i zatwierdź enterem (0, i enter aby zakończyć wybór): "))
+            choice = int(input("Choose a number/s and press enter. To finish press 0 and enter: "))
             if choice == 0:
                 break
             elif choice in options:
                 selected_options.append(choice)
-                print(f"Wybrano: {options[choice]['description']} - {options[choice]['price']} zł")
+                print(f"Choose: {options[choice]['description']} - {options[choice]['price']} zł")
             else:
-                print("Nieprawidłowy numer opcji. Spróbuj ponownie.")
+                print("Incorrect. Try again.")
         except ValueError:
-            print("Wprowadź poprawny numer.")
+            print("Type correct number/s.")
     return selected_options
 
 def calculate_nfz_addons_cost(selected_options, options):
@@ -62,3 +62,75 @@ dha = "https://www.doz.pl/apteka/p56540-Pregna_DHA_kapsulki_30_szt."
 jod1 = "https://www.doz.pl/apteka/p136644-Femibion_1_Wczesna_ciaza_tabletki_powlekane_28_szt."
 jod2 = "https://www.doz.pl/apteka/p121240-Prenatal_Duo_II_i_III_trymestr_ciazy_kapsulki_30_szt._Classic__60_szt._DHA"
 
+class Child_budget:
+    def __init__(self, stage):
+        self.stage = stage
+        self.expenses = {}
+
+    def add_expenses(self, category, cost):
+        category = str(category)
+        self.expenses[category] = cost  
+
+    def __str__(self):     
+        return (f"{self.expenses.items()}, total:{self.expenses.values()}")
+    
+    #wyprawka noworodka: 
+newborn = {
+    "crib" : 720,
+    "mattress" : 270,
+    "changing_table" : 170,
+    "stroller_set" : 3500,
+    "linen" : 85,
+    "cloth_diapers" : 25,
+    "blanket" : 75
+    }
+#Niemowlę(0-12) 
+toddler = {
+    "nappies" : 2400,
+    "bottle_feeding" : 5100,
+    "cosmetics" : 680,
+    "vaccination" : 3260,
+    "clothes" : 1900,
+    "chair" : 300,
+    "toys" : 1700
+}
+#Małe dziecko(1-3)
+older_toddler = {
+    "food" : 4000,
+    "bottle_feeding" : 1000,
+    "nappies" : 1200,
+    "vaccines" : 1400,
+    "clothes" : 2200,
+    "toys" : 2400, 
+    "nursery" : 36000
+}
+#preschool(4-6)
+preschool = {
+    "public_preschool" : 7400,
+    "private_preschool" : 36000,
+    "clothes" : 7500,
+    "food" : 4000 
+}
+#child(7-12)
+child = {
+    "public_school" : 16000,
+    "private_school" : 135000,
+    "additional_class" : 10000,
+    "transportation" : 15000,
+    "hobby" : 4500,
+    "phone" : 3000,
+    "computer" : 4500,
+    "clothes" : 10000,
+    "food" : 25000
+}
+#teen(13-18)
+teen = {
+    "public_school" : 27000,
+    "private_school" : 145000,
+    "additional_class" : 15000,
+    "transportation" : 18000,
+    "hobby" : 6000,
+    "phone" : 3000,
+    "computer" : 4500,
+    "clothes" : 15000,
+    "food" : 30000}
